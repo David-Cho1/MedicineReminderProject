359449554c09dd3cdbe77d17c706539f79cbd686
choid <choid@stu.otc.school.nz>
2024-06-07 09:55:45 +1200

    Signup Page and DatabaseHelper added 07/06/2024

---
+++ app/src/main/java/com/example/medicinereminderproject/DatabaseHelper.java
@@ -0,0 +1,68 @@
+package com.example.medicinereminderproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    public static final String databaseName = "Signup.db";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "Signup.db", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase MyDatabase) {
+        MyDatabase.execSQL("create Table allusers(email TEXT primary key, password TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase MyDatabase, int oldVersion, int newVersion) {
+        MyDatabase.execSQL("drop Table if exists allusers");
+    }
+
+    public Boolean insertData(String email, String password) {
+        SQLiteDatabase MyDatabase =  this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email", email);
+        contentValues.put("password", password);
+        long result = MyDatabase.insert("allusers", null, contentValues);
+
+        if (result == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+    public Boolean checkEmail(String email) {
+        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+        Cursor cursor = MyDatabase.rawQuery("Select * from allusers where email = ?", new String[]{email});
+
+        if (cursor.getCount() > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public Boolean checkEmailPassword(String email, String password) {
+        SQLiteDatabase MyDatabase = this.getWritableDatabase();
+        Cursor cursor = MyDatabase.rawQuery("Select * from allusers where email = ?", new String[]{email, password});
+
+        if (cursor.getCount() > 0) {
+            return true;
+        }
+        else {
+            return false;
+
+
+        }
+    }
+}
--- app/src/main/java/com/example/medicinereminderproject/LoginPage.java
+++ app/src/main/java/com/example/medicinereminderproject/LoginPage.java
@@ -1,22 +1,13 @@
 package com.example.medicinereminderproject;
 
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
 
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.room.Room;
-
-
-import com.google.android.material.textfield.TextInputEditText;
 
 
 public class LoginPage extends AppCompatActivity {
@@ -25,8 +16,19 @@ public class LoginPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_page);
+        setContentView(R.layout.activity_login);
+        setInit();
 
+    }
+    private void setInit() {
+        // Listen to the Text View and when it's clicked, send to the SignUp Page
+        TextView sendSignUpbtn = (TextView)findViewById(R.id.signUpText);
+        sendSignUpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginPage.this, AlarmSetPage.class));
+            }
+        });
     }
 }
 
