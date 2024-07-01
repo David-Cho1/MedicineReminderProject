26050e74b36a2a0ef793fdb07ae752e01c646a44
choid <choid@stu.otc.school.nz>
2024-07-02 11:00:30 +1200

    Login page done  / main page done 27/06/2024

---
+++ app/src/main/java/com/example/medicinereminderproject/AlarmPage.java
@@ -0,0 +1,21 @@
+package com.example.medicinereminderproject;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AlarmPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_alarm);
+
+
+    }
+}
--- app/src/main/java/com/example/medicinereminderproject/MainPage.java
+++ app/src/main/java/com/example/medicinereminderproject/MainPage.java
@@ -1,6 +1,11 @@
 package com.example.medicinereminderproject;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +17,24 @@ public class MainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        ImageButton alarmButton = findViewById(R.id.alarmButton);
+        ImageButton diaryButton = findViewById(R.id.diaryButton);
+
+        alarmButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AlarmPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+        diaryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), DiaryPage.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
