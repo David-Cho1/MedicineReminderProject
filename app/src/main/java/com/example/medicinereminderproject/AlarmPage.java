53a77df10a82b085461eb07dfb2136dd2d35b4aa
choid <choid@stu.otc.school.nz>
2024-07-31 22:16:54 +1200

    Dialog pop ups + Acitivity Alarmset changed

--- app/src/main/java/com/example/medicinereminderproject/AlarmPage.java
+++ app/src/main/java/com/example/medicinereminderproject/AlarmPage.java
@@ -1,13 +1,18 @@
 package com.example.medicinereminderproject;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -38,9 +43,29 @@ public class AlarmPage extends AppCompatActivity {
         btn_write.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("Clicked", "True");
                 // Pop up page open
                 Dialog dialog = new Dialog(AlarmPage.this, android.R.style.Theme_Material_Light_Dialog);
                 dialog.setContentView(R.layout.dialog_alarm);
+
+                // Buttons in Dialog
+                Button newBtn = dialog.findViewById(R.id.newButton);
+                Button deleteBtn = dialog.findViewById(R.id.deleteButton);
+
+                // Show Dialog
+                dialog.show();
+
+                // If new button is pressed
+                newBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    // Open AlarmSet page
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                        Intent intent = new Intent(getApplicationContext(), AlarmSetPage.class);
+                        startActivity(intent);
+                    }
+                });
+
             }
         });
     }
--- app/src/main/java/com/example/medicinereminderproject/AlarmSetPage.java
+++ app/src/main/java/com/example/medicinereminderproject/AlarmSetPage.java
@@ -15,14 +15,7 @@ import java.util.Calendar;
 
 // Main Class
 public class AlarmSetPage extends AppCompatActivity {
-
-
-
-
-
-
     // TIME PICKING
-
 
     // Creating Variable
     private Button pickTimeBtn;
@@ -35,19 +28,12 @@ public class AlarmSetPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarmset);
 
-
-
-
-
-
         // Putting the buttons and text from the XML file into a variables
         pickTimeBtn = findViewById(R.id.idBtnPickTime);
         selectedTimeTV = findViewById(R.id.idTVSelectedTime);
 
-
         // Listen when the button is clicked
         pickTimeBtn.setOnClickListener(new View.OnClickListener() {
-
 
             // Function that fixes glitch where it preceding 0
             public String checkDigit(int number) {
@@ -55,41 +41,27 @@ public class AlarmSetPage extends AppCompatActivity {
                 return number <= 9 ? "0" + number : String.valueOf(number);
             }
 
-
             // Function that gets the Time from the Calendar
             @Override
             public void onClick(View v) {
                 final Calendar c = Calendar.getInstance();
 
-
                 // Set integer variables to the separated time parts
                 int hour = c.get(Calendar.HOUR_OF_DAY);
                 int minute = c.get(Calendar.MINUTE);
 
-
                 TimePickerDialog timePickerDialog = new TimePickerDialog(AlarmSetPage.this,
                         new TimePickerDialog.OnTimeSetListener() {
 
-
                             // Function that changes the TextView to the Setted time
                             @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay,
-                                                  int minute) {
-
-
-                                selectedTimeTV.setText(checkDigit(hourOfDay) + ":"
-                                        + checkDigit(minute));
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                selectedTimeTV.setText(checkDigit(hourOfDay) + ":" + checkDigit(minute));
                             }
                         }, hour, minute, false);
                 timePickerDialog.show();
             }
         });
     }
-
-
-
-
-
-
 }
 
